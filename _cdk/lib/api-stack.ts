ddb4f77552eae89dddf90b6ba917fa17a0f24b29
Adam Quigley <adamjohnquigley@gmail.com>
2022-03-04 10:38:13 +1100

    Upgrade to CDK V2. Add linting (#2)

--- _cdk/lib/api-stack.ts
+++ _cdk/lib/api-stack.ts
@@ -1,31 +1,32 @@
-import { Construct, Duration, Stack, StackProps } from '@aws-cdk/core';
-import { Code, Function, Runtime } from '@aws-cdk/aws-lambda';
-import { LambdaRestApi } from '@aws-cdk/aws-apigateway';
-import { AttributeType, BillingMode, Table } from '@aws-cdk/aws-dynamodb';
+import { Construct } from "constructs";
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
+import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
 
-const LAMBDA_RUNTIME = Runtime.GO_1_X
-const LAMBDA_DEFAULT_TIMEOUT = Duration.seconds(30)
-const LAMBDA_DEFAULT_MEMORY = 256 // megabytes
-const CODE_DIST = "../dist"
+const LAMBDA_RUNTIME = Runtime.GO_1_X;
+const LAMBDA_DEFAULT_TIMEOUT = Duration.seconds(30);
+const LAMBDA_DEFAULT_MEMORY = 256; // megabytes
+const CODE_DIST = "../dist";
 
 export class ApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // follow single-table design pattern
-    const ddbTable = new Table(this, 'DynamoTable', {
+    const ddbTable = new Table(this, "DynamoTable", {
       billingMode: BillingMode.PAY_PER_REQUEST,
       partitionKey: {
-        name: 'PK',
+        name: "PK",
         type: AttributeType.STRING,
       },
       sortKey: {
-        name: 'SK',
+        name: "SK",
         type: AttributeType.STRING,
-      }
+      },
     });
 
-    const backend = new Function(this, 'ApiLambdaBackend', {
+    const backend = new Function(this, "ApiLambdaBackend", {
       runtime: LAMBDA_RUNTIME,
       handler: "api",
       code: Code.fromAsset(CODE_DIST),
@@ -33,12 +34,12 @@ export class ApiStack extends Stack {
       memorySize: LAMBDA_DEFAULT_MEMORY,
       environment: {
         BACKEND_TABLE: ddbTable.tableName,
-      }
+      },
     });
 
     // proxy requests to lambda by default
-    const api = new LambdaRestApi(this, 'Api', {
-      handler: backend
+    new LambdaRestApi(this, "Api", {
+      handler: backend,
     });
 
     // IAM
